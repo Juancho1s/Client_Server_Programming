183daa78e85c0f7d91e35c02568379e755aed36f
Juancho1s <113451623+Juancho1s@users.noreply.github.com>
2023-05-17 10:34:11 -0600

    This is the quicksort finished

---
+++ PersonalPractices/QuickSort.rb
@@ -0,0 +1,52 @@
+def quicksort(array)
+    if (array.length == 1)
+      return
+    end
+    startP = 0;
+    endP = array.length - 1;
+    sorting(array, startP, endP)
+end
+
+def sorting (array, startP, endP)
+  if (startP >= endP)
+    return  
+  end
+
+  pivot = array[endP]
+  startPAux = startP
+  endPAux = endP
+
+  while (endPAux > startPAux)
+    while (array[startPAux] <= pivot && endPAux > startPAux)
+      startPAux += 1
+    end
+
+    while (array[endPAux] >= pivot && endPAux > startPAux)
+      endPAux -= 1
+    end
+
+    switching(array, startPAux, endPAux)
+  end
+
+  switching(array, startPAux, endP)
+  
+  sorting(array, startP, startPAux - 1)
+  sorting(array, startPAux + 1, endP)
+end
+
+def switching (array, index_1, index_2)
+  aux = array[index_1]
+  array[index_1] = array[index_2]
+  array[index_2] = aux
+end
+
+
+array = []
+
+for i in 0..19
+  array.push(rand(100))
+end
+
+quicksort(array)
+
+print(array)
