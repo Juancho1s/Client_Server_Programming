bd27896a5ea508a8746c6d36a3e8da83cabb9ad4
Juan Valenzuela <113451623+Juancho1s@users.noreply.github.com>
2023-07-04 16:33:29 -0600

    Delete ExpressProjects/porto_3.7 directory

--- ExpressProjects/porto_3.7/porto_3.7/HTML/js/app.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $(".portal-menu").on("click", function () {
-        var ref = $(this).attr("data-menu");
-        $('html, body').scrollTo(ref, 325, { offset: { top: -150 }});
-    });
-});
